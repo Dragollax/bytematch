22181f85bf091f724918227ed3ec11e2d2dc65b0
_Kevin_Zhang_ <49049813+Dragollax@users.noreply.github.com>
2022-06-16 10:14:43 -0700

    adjust algorithm
    made algorithm into a function that can accept data, instead of the test data that it uses by default

--- match.py
+++ match.py
@@ -1,77 +1,5 @@
 from transform import transform_user
 import copy
-test2 = {
-    "3": [
-        {
-            "id": 32589021,
-            "username": "dragolla",
-            "profile_image": "https://weco-static.s3.amazonaws.com/profilepicture.jpg",
-            "interests": [
-                1,
-                3
-            ],
-            "biography": "",
-            "tags": [
-                "swimming"
-            ]
-        }
-    ],
-    "5": [
-        {
-            "id": 578172410,
-            "username": "teststs",
-            "profile_image": "https://weco-static.s3.amazonaws.com/profilepicture.jpg",
-            "interests": [
-                5
-            ],
-            "biography": "",
-            "tags": [
-                "coding"
-            ]
-        }
-    ]
-}  
-test = [
-        {
-            "id": 32589021,
-            "username": "dragolla",
-            "profile_image": "https://weco-static.s3.amazonaws.com/profilepicture.jpg",
-            "interests": [
-                1,
-                3
-            ],
-            "biography": "",
-            "tags": [
-                "swimming"
-            ]
-        }
-    ,
-        {
-            "id": 578172410,
-            "username": "teststs",
-            "profile_image": "https://weco-static.s3.amazonaws.com/profilepicture.jpg",
-            "interests": [
-                5
-            ],
-            "biography": "",
-            "tags": [
-                "coding"
-            ]
-        }
-]
-_self = {
-            "id": 578172410,
-            "username": "teststs",
-            "profile_image": "https://weco-static.s3.amazonaws.com/profilepicture.jpg",
-            "interests": [
-                5
-            ],
-            "biography": "",
-            "tags": [
-                "coding"
-            ]
-}
-
 
 passed = []
 once = False
@@ -80,56 +8,59 @@ class Match:
 
         matches = []
 
-        matchedUsers = [] 
+        match_data = kwargs[0]
+
+        target = args[0]
+        
+        matchedData = [] 
 
 
         #return a dict of keys that are the user's interests
         modified_user = transform_user(_self, 'interest')   
 
-        for user in test: #NOTE: change this later!!
-            matchedInterests=[]
-            for userInterest in user['interests']:
+        for user in match_data: #NOTE: change this later!!
+            matched=[]
+            for attribute in user.get('interests'):
                 #will compare key from modified_user with userInterest    
                 #this will prevent us from having another nested for loop and waste time iterating through the user's interest every time
-                if userInterest in modified_user: 
-                    matchedInterests.append(userInterest)
-                    user['matchedPercentage']=((len(matchedInterests)+1)/(len(user['interests'])+2)+(len(matchedInterests)+1)/(len(_self.get('interests'))+2))/2
-                    user['matchedInterests']=matchedInterests
-                    matchedUsers.append(user)
+                if attribute in target: 
+                    matched.append(attribute)
+                    user['matchedPercentage']=((len(matched)+1)/(len(user.get('interests'))+2)+(len(matched)+1)/(len(target)+2))/2
+                    user['matchedAttributes']=matched
+                    matchedData.append(user)
                     break
-        matchedUsers.sort(key=lambda x: x.get('matchedPercentage'), reverse=True)
+        matchedData.sort(key=lambda x: x.get('matchedPercentage'), reverse=True)
 
-        for x in matchedUsers: #to remove duplicate dictionaries from list
+        for x in matchedData: #to remove duplicate dictionaries from list
             if x not in matches:
                 matches.append(x)
-        #if not enough users, use recursion to get enough users, and append the existing users into passed
-
         results = {
             "matched": matches,
         }
         return results
+        
     def quick_match(*args,  **kwargs):
         #data group for matching
-        #match_data = kwargs[0]
-        #target for matching
-        #target = args[0]
+        match_data = kwargs[0]
+        #target for matching, shouyld be in list data structure
+        target = args[0]
         i = 0 #will be used to iterate through interests
         j = 0 #will be used to iterate through the current user's interests
         once = False#just ot make sure we only assign inverted_group's length to length once
         length = 0
         raw_matches = []
         #this will stop the algo if we have reached the end of the user's interests. 
-        while i < len(_self.get('interests')) or j < length: 
+        while i < len(target) or j < length: 
             #matchedInterests = []
             if once == False:
                 once = True
                 length = len(test2)
             
-            if int(_self.get('interests')[i]) == int(list(test2.keys())[j]): #if the j'th value of inverted_group is equal to the i'th value of current_user
+            if int(target[i]) == int(list(match_data.keys())[j]): #if the j'th value of inverted_group is equal to the i'th value of current_user
                 j += 1
-                users = test2.get(str(_self.get('interests')[i]))
+                matched = match_data.get(str(target[i]))
 
-                raw_matches.extend(users)
+                raw_matches.extend(matched)
 
                     
             else: # if no matches, then move on onto the next interest in inverted_group
--- tests.py
+++ tests.py
@@ -31,6 +31,34 @@ test = {
         }
     ]
 }  
+test2 = [
+        {
+            "id": 32589021,
+            "username": "dragolla",
+            "profile_image": "https://weco-static.s3.amazonaws.com/profilepicture.jpg",
+            "interests": [
+                1,
+                3
+            ],
+            "biography": "",
+            "tags": [
+                "swimming"
+            ]
+        }
+    ,
+        {
+            "id": 578172410,
+            "username": "teststs",
+            "profile_image": "https://weco-static.s3.amazonaws.com/profilepicture.jpg",
+            "interests": [
+                5
+            ],
+            "biography": "",
+            "tags": [
+                "coding"
+            ]
+        }
+]
 __self = {
         "5": [
         {
